e05a4ffb2c78364fcb96a61f3a2b0ba16fe30d42
박지원 <cute060500@naver.com>
2021-10-04 13:06:22 +0900

    SongActivity&layout

--- app/src/main/java/com/example/flo/MainActivity.kt
+++ app/src/main/java/com/example/flo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.flo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flo.databinding.ActivityMainBinding
@@ -8,9 +9,15 @@ import com.example.flo.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.mainPlayerLayout.setOnClickListener {
+            startActivity(Intent(this, SongActivity::class.java))
+        }
+
         initNavigation()
 
         binding.mainBnv.setOnItemSelectedListener {
